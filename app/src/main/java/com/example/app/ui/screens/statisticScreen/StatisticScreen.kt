e5582a85afc79807cc68019efa2e6bb34305d017
Admin <Evgeniy@>
2025-12-04 14:22:20 +0300

    the statistics screen has been drawn

--- app/src/main/java/com/example/app/ui/screens/statisticScreen/StatisticScreen.kt
+++ app/src/main/java/com/example/app/ui/screens/statisticScreen/StatisticScreen.kt
@@ -1,21 +1,126 @@
 package com.example.app.ui.screens.statisticScreen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.app.ui.components.GeneralButton
+import com.example.app.ui.theme.TaskSchedulerTheme
+import androidx.compose.foundation.layout.Box
 
 @Composable
 fun StatisticScreen(
     onBack: () -> Unit
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize(),
-    ) {
-        Text(
-            text = "Statistic Screen"
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            BottomAppBar(
+                modifier = Modifier.height(130.dp),
+                windowInsets = WindowInsets(0.dp),
+                containerColor = Color.Transparent
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    GeneralButton(
+                        onClick = {}
+                    ) {
+                        Text(
+                            text = "Сбросить статистику",
+                            fontSize = 20.sp,
+                        )
+                    }
+                    GeneralButton(
+                        onClick = {
+                            onBack()
+                        }
+                    ) {
+                        Text(
+                            text = "Назад",
+                            fontSize = 20.sp,
+                        )
+                    }
+                }
+            }
+        }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(bottom = 130.dp)
+                    .statusBarsPadding()
+                    .padding(horizontal = 40.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = "Создано задач")
+                    Text(text = "10")
+                }
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = "Выполнено")
+                    Text(text = "45")
+                }
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = "Отменено")
+                    Text(text = "34")
+                }
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = "Процент выполенения")
+                    Text(text = "87%")
+                }
+
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun StatisticScreenPreview() {
+    TaskSchedulerTheme {
+        StatisticScreen(
+            onBack = {},
         )
     }
 }
